9113adc20eb948745c35e9857d7c431c12549508
zehamless <82883040+zehamless@users.noreply.github.com>
2023-02-24 15:34:05 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Lato.css">
+    <link rel="stylesheet" href="assets/css/Montserrat.css">
+    <link rel="stylesheet" href="assets/css/Open%20Sans.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/Section-Title.css">
+    <link rel="stylesheet" href="assets/css/timeline_12073_SaraSantos.css">
+    <link rel="stylesheet" href="assets/css/untitled.css">
+</head>
+
+<body id="page-top" data-bs-spy="scroll" data-bs-target="#mainNav" data-bs-offset="72">
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-secondary text-uppercase" id="mainNav">
+        <div class="container"><a class="navbar-brand" href="#page-top">Portofolio</a><button data-bs-toggle="collapse" data-bs-target="#navbarResponsive" class="navbar-toggler text-white bg-primary navbar-toggler-right text-uppercase rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#riwayat">Riwayat</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header class="text-center text-white bg-primary masthead">
+        <div class="container"><img class="img-fluid d-block mx-auto mb-5 rounded " src="assets/img/img.jpg" style="width: auto; height: 300px;">
+            <h1>Mahez Pradana</h1>
+            <h2 class="font-weight-light mb-0">2015061070</h2>
+        </div>
+    </header>
+    <section id="portfolio" class="portfolio">
+        <div class="container">
+            <h2 class="text-uppercase text-center text-secondary">ME<br></h2>
+            <hr>
+            <div class="row">
+                <div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" style="font-size: 46px;">
+                        <path d="M21 15.5458C20.4771 15.5458 19.9542 15.6972 19.5 16C18.5917 16.6056 17.4083 16.6056 16.5 16C15.5917 15.3944 14.4083 15.3944 13.5 16C12.5917 16.6056 11.4083 16.6056 10.5 16C9.59167 15.3944 8.40833 15.3944 7.5 16C6.59167 16.6056 5.40833 16.6056 4.5 16C4.04584 15.6972 3.52292 15.5458 3 15.5458M9 6V8M12 6V8M15 6V8M9 3H9.01M12 3H12.01M15 3H15.01M21 21V14C21 12.8954 20.1046 12 19 12H5C3.89543 12 3 12.8954 3 14V21H21ZM18 12V10C18 8.89543 17.1046 8 16 8H8C6.89543 8 6 8.89543 6 10V12H18Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg><span>Ttl: Pajaresuk, 25 Juli 2001</span></div>
+                <div class="col"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" style="font-size: 46px;">
+                        <path d="M3 8L10.8906 13.2604C11.5624 13.7083 12.4376 13.7083 13.1094 13.2604L21 8M5 19H19C20.1046 19 21 18.1046 21 17V7C21 5.89543 20.1046 5 19 5H5C3.89543 5 3 5.89543 3 7V17C3 18.1046 3.89543 19 5 19Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+                    </svg><span>Email: batubattery25@gmail.com</span></div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6"><i class="fa fa-home" style="font-size: 46px;"></i><span>Alamat: Pajaresuk, Pringsewu</span></div>
+                <div class="col-md-6"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 20 20" fill="none" style="font-size: 46px;">
+                        <path d="M2 3C2 2.44772 2.44772 2 3 2H5.15287C5.64171 2 6.0589 2.35341 6.13927 2.8356L6.87858 7.27147C6.95075 7.70451 6.73206 8.13397 6.3394 8.3303L4.79126 9.10437C5.90756 11.8783 8.12168 14.0924 10.8956 15.2087L11.6697 13.6606C11.866 13.2679 12.2955 13.0492 12.7285 13.1214L17.1644 13.8607C17.6466 13.9411 18 14.3583 18 14.8471V17C18 17.5523 17.5523 18 17 18H15C7.8203 18 2 12.1797 2 5V3Z" fill="currentColor"></path>
+                    </svg><span>No: 089632172873</span></div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <div class="row"></div>
+                <hr>
+            </div>
+        </div>
+        <div class="container" style="position: relative;display: block;">
+            <div class="row">
+                <div class="col-xxl-3">
+                    <h1 style="border-right-width: 3px;border-right-style: solid;">Mata Kuliah Favorit:</h1>
+                </div>
+                <div class="col">
+                    <h1>Embedded System</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="text-white bg-primary mb-0" id="riwayat">
+        <div class="container">
+            <h2 class="text-uppercase text-center text-white">Riwayat sekolah</h2>
+            <hr>
+            <div class="row">
+                <div class="col">
+                    <div class="timeline time-body">
+                        <div class="time-container left">
+                            <div class="time-content">
+                                <h2 style="color: #474e5d;">SDN1 Pajaresuk</h2>
+                            </div>
+                        </div>
+                        <div class="time-container right">
+                            <div class="time-content">
+                                <h2 style="color: #474e5d;">SMPN1 Pringsewu</h2>
+                            </div>
+                        </div>
+                        <div class="time-container left">
+                            <div class="time-content">
+                                <h2 style="color: #474e5d;">SMAN1 Pringsewu</h2>
+                            </div>
+                        </div>
+                        <div class="time-container right">
+                            <div class="time-content">
+                                <h2 style="color: #474e5d;">UNILA<br>Teknik Informatika</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <div class="text-center text-white copyright py-4">
+        <div class="container"><small>bootstrapStudio</small></div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/java.js"></script>
+</body>
+
+</html>
